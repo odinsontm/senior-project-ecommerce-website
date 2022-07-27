06a1ccb10a3e64aa591398d80a330c921eee1e3c
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2022-07-27 16:18:21 -0600

    created backend API

---
+++ backend/data.js
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      name: 'stylish shirt',
+      slug: 'stylish-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 100,
+      description: 'cool shirt, much wow, awesome!',
+    },
+    {
+      name: 'stylish hoodie',
+      slug: 'stylish-hoodie',
+      category: 'hoods',
+      image: '/images/p2.jpg',
+      price: 120,
+      description: 'less cool shirt, much wow, awesome!',
+    },
+    {
+      name: 'stylish pant',
+      slug: 'stylish-pant',
+      category: 'Shorts',
+      image: '/images/p3.jpg',
+      price: 50,
+      description: 'cool short, much wow, awesome!',
+    },
+  ],
+};
+
+export default data;
