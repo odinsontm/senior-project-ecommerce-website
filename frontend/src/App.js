d2a1992ce790db1652762429d3ffb8538168eb27
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2022-09-25 17:54:16 -0600

    created react context for add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,35 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
-              {/* MENU BUTTON HERE */}
               <LinkContainer to="/">
                 <Navbar.Brand>TheDEFT</Navbar.Brand>
               </LinkContainer>
-              {/* ACCOUNT HERE */}
-              {/* CART HERE */}
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
