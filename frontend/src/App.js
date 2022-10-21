20bb7db2ffed39028ff0e5954685ed98ef8e3beb
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2022-10-21 17:40:39 -0600

    fixing nav styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <BrowserRouter>
       {/* Navigation for prod */}
-      <div class="site-container background">
+      <div class="site-container">
         <ToastContainer position="bottom-center" limit={1} />
         <Fragment>
           <Navbar />
