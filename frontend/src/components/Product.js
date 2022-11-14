de3d799bda3fc5f8da11ac0b0dec909673aab795
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2022-11-14 13:44:10 -0700

    updated database

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -26,27 +26,6 @@ function Product(props) {
       payload: { ...item, quantity },
     });
   };
-  /*
-  return (
-    <Card>
-      <Link to={`/product/${product.slug}`}>
-        <img src={product.image} className="card-img-top" alt={product.name} />
-      </Link>
-      <Card.Body>
-        <Link to={`/product/${product.slug}`}>
-          <Card.Title>{product.name}</Card.Title>
-        </Link>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>${product.price}</Card.Text>
-        {product.countInStock === 0 ? (
-          <Button variant="light" disabled>
-            Out of Stock
-          </Button>
-        ) : (
-          <Button onClick={() => addToCartHandler(product)}>Add To Cart</Button>
-        )}
-      </Card.Body>
-    </Card>*/
 
   return (
     <div
